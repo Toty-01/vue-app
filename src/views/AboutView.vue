f1b5dd4eaf911fca6bf1826c8b7693ef3ad3406c
Toty-01 <caucheanthony@gmail.com>
2022-09-15 19:22:41 +0200

    calculatrice ok

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="about">
     <h1>Calculatrice</h1>
+
+    <div class="calc">
     <div class="p-3">
 
       <div class="w-full rounded m-1 p-3 text-right lead font-weight-bold text-white bg-vue-dark">
@@ -11,8 +13,8 @@
           <div class="col-3" v-for="n in calculatorElements" :key="n">
 
             <div class="lead text-white text-center mt-2 py-3 bg-vue-dark rounded hover-class"
-              :class="{'bg-vue-green': ['C','x','/','-','+','%','='].includes(n)}"
-            >
+              :class="{'bg-vue-green': ['C','*','/','-','+','%','='].includes(n)}"
+            @click="action(n)">
               {{n}}
             </div>
 
@@ -20,15 +22,48 @@
         </div>
     </div>
   </div>
+  </div>
 </template>
 
 <script>
+import { thisExpression } from '@babel/types';
+
 export default {
   name: 'AboutView',
   data() {
     return {
       calculatorValue: '',
-      calculatorElements:['C','x','/','-',7,8,9,'+',4,5,6,'%',1,2,3,'=',0,'.'],
+      calculatorElements:['C','*','/','-',7,8,9,'+',4,5,6,'%',1,2,3,'=',0,'.'],
+      operator: null,
+      previousCalculatorValue: '',
+    }
+  },
+  methods: {
+    action(n){
+      if(!isNaN(n) || n === '.'){
+        this.calculatorValue += n + '';
+      }
+      /*Clear Value*/
+      if(n === 'C'){
+        this.calculatorValue = '';
+      }
+      /*Modulo Value*/
+      if(n === '%'){
+        this.calculatorValue = this.calculatorValue / 100 + '';
+      }
+      if(['/','*','-','+'].includes(n)){
+        this.operator = n;
+        this.previousCalculatorValue = this.calculatorValue;
+        this.calculatorValue = '';
+      }
+      /*Egal Value*/
+      if(n === '='){
+        this.calculatorValue = eval(
+          this.previousCalculatorValue + this.operator + this.calculatorValue
+        );
+        this.previousCalculatorValue = '',
+        this.operator = null;
+      }
     }
   }
 }
