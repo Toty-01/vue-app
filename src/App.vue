715d20f0b1f7c895110552058e07a2afd4bf1802
Toty-01 <caucheanthony@gmail.com>
2022-09-15 03:02:56 +0200

    calculatrice projet

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link> |
+    <router-link to="/about">Calculatrice</router-link> |
     <router-link to="/galerie">Galerie</router-link> |
     <router-link to="/contact">Contact</router-link>
   </nav>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,56 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Calculatrice</h1>
+    <div class="p-3">
+
+      <div class="w-full rounded m-1 p-3 text-right lead font-weight-bold text-white bg-vue-dark">
+        {{ calculatorValue || 0 }}
+      </div>
+
+        <div class="row no-gutters">
+          <div class="col-3" v-for="n in calculatorElements" :key="n">
+
+            <div class="lead text-white text-center mt-2 py-3 bg-vue-dark rounded hover-class"
+              :class="{'bg-vue-green': ['C','x','/','-','+','%','='].includes(n)}"
+            >
+              {{n}}
+            </div>
+
+          </div>
+        </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: 'AboutView',
+  data() {
+    return {
+      calculatorValue: '',
+      calculatorElements:['C','x','/','-',7,8,9,'+',4,5,6,'%',1,2,3,'=',0,'.'],
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .p-3 {
+    max-width: 400px;
+    margin: 50px auto;
+    background-color: #234;
+  }
+  .w-full {
+    text-align: end;
+  }
+  .bg-vue-dark {
+   background: #31475e;
+  }
+  .hover-class:hover {
+    cursor: pointer;
+    background-color: #3D5875;
+  }
+  .bg-vue-green {
+    background: #3fb984;
+  }
+</style>
